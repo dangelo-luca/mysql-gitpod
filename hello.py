5697ad87184bed00a4c0b9e92c81c8be035c8b8e
Luca D'Angelo <luca.dangelo06@gmail.com>
2025-05-07 07:46:33 +0000

    fatto delle prove con mysql

--- hello.py
+++ hello.py
@@ -1,10 +1,34 @@
-# save this as hello.py
-from flask import Flask
+import mysql.connector
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database = "events"
+)
 
 
-app = Flask(__name__)
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE IF NOT EXISTS events")
+
+mycursor.execute("SHOW DATABASES")
 
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+for x in mycursor:
+  print(x)
+
+mycursor.execute("CREATE TABLE IF NOT EXISTS customers (name VARCHAR(255), address VARCHAR(255))")
+mycursor.execute("SHOW TABLES")
+
+for x in mycursor:
+  print(x)
+
+mycursor.execute("SELECT * FROM customers")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
